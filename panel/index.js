ecbdef5d984bfab695c23ee24be3e108c8e57a45
fusijie <fusijie@vip.qq.com>
2019-01-30 18:31:58 +0800

    update

--- panel/index.js
+++ panel/index.js
@@ -9,28 +9,41 @@ Editor.Panel.extend({
     template: Fs.readFileSync(Editor.url("packages://excel2json/panel/index.html"), "utf-8"),
 
     $: {
-        btn_update_excel: '#btn-update-excel',
     },
 
     ready() {
-        this.$btn_update_excel.addEventListener('confirm', () => {
-            let localExcelDir = "excel";
-            Editor.Ipc.sendToMain('excel2json:update-excel', localExcelDir, (err, data) => {
-                if (err) {
-                    Editor.log(err);
-                    return;
-                }
-                for (let i = 0; i < data.length; i++) {
-                    if (Path.extname(data[i]) === ".xlsx") {
-                        Editor.log(Path.join(Editor.projectInfo.path, localExcelDir, data[i]));
-                    }
-                }
-            });
+        new window.Vue({
+            el: this.shadowRoot,
+            data: {
+                txtUpdate: '点击更新Excel',
+                items: [
+                ]
+            },
+            methods: {
+                onConfirm(event) {
+                    event.stopPropagation();
+                    let localExcelDir = "excel";
+                    Editor.Ipc.sendToMain('excel2json:update-excel', localExcelDir, (err, data) => {
+                        if (err) {
+                            Editor.log(err);
+                            return;
+                        }
+                        this.items = [];
+                        for (let i = 0; i < data.length; i++) {
+                            if (Path.extname(data[i]) === ".xlsx") {
+                                this.items.push({
+                                    message: Path.join(Editor.projectInfo.path, localExcelDir, data[i])
+                                });
+                            }
+                        }
+                    });
+                },
+            },
         });
     },
 
     messages: {
-        'greeting' (event, question) {
+        'greeting'(event, question) {
             Editor.log(question);
             if (event.reply) {
                 //if no error, the first argument should be null
